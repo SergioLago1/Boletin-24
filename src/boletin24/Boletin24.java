a64392e0b52d310f3107f2b29b64dbbf2fffddb4
julianMGCODE <julianmosquerag@gmail.com>
2016-04-07 11:50:37 +0200

    FIN

---
+++ src/boletin24/Boletin24.java
@@ -0,0 +1,16 @@
+
+package boletin24;
+
+/**
+ *
+ * @author julian
+ */
+public class Boletin24 {
+
+    
+    public static void main(String[] args) {
+        Marco inter= new Marco();
+           inter.setVisible(true);
+    }
+    
+}
